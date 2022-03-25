84002c9e3941641c1269efff8cd1abb0453a93eb
YudakovVV <skiff2140@gmail.com>
2022-03-25 22:14:46 +0400

    выполнено задание 2

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
+let title = "lesson02";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 200;
+let rollback = 20;
+let fullPrice = 30000;
+let adaptive = true;
+let lowerCase = screens.toLowerCase();
+let arr = lowerCase.split(",");
+let persInter = fullPrice * (rollback / 100);
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(
+  "Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани"
+);
+console.log(arr);
+console.log(persInter);
 
