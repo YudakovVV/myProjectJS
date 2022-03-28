0c2430ecd9d71f4eee40b38ddce51971e96131ff
YudakovVV <skiff2140@gmail.com>
2022-03-28 21:33:01 +0400

    решение задания 3

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
+let lang = prompt("Введите язык отображения", "ru,en");
+if (lang == "ru"){
+  arr = console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
+}
+else console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+
+lang = prompt("Введите язык отображения", "ru,en");
+switch(lang){
+  case "ru": 
+    console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
+    break;
+  case "en": 
+    console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+    break;
+}
+
+lang = prompt("Введите язык отображения", "ru,en");
 
 
+let namePerson = prompt("Введите имя", "Артем, Александр");
+let resalt = (namePerson == "Артем") ? "директор": (namePerson == "Александр") ? "учитель" : "студент";
+console.log(resalt);
