6534cb207a6bc395ef5307057c571ec81e9dec70
YudakovVV <skiff2140@gmail.com>
2022-03-28 21:55:30 +0400

    дополнение к заданию

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 let lang = prompt("Введите язык отображения", "ru,en");
+let arr;
+let numDey = +prompt("Введите номер дня недели", "1 - 7");
 if (lang == "ru"){
-  arr = console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
+  lang = "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье";
+  console.log(lang);
+  arr = lang.split(", ");
+  numDey = numDey - 1;
+  console.log(arr[numDey]);
+  
 }
-else console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+else 
+  lang = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday";
+  console.log(lang);
+  arr = lang.split(", ");
+  numDey = numDey - 1;
+  console.log(arr[numDey]); 
 
 lang = prompt("Введите язык отображения", "ru,en");
+numDey = +prompt("Введите номер дня недели", "1 - 7");
 switch(lang){
   case "ru": 
-    console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
+    lang = "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье";
+    console.log(lang);
+    arr = lang.split(", ");
+    numDey = numDey - 1;
+    console.log(arr[numDey]);
     break;
   case "en": 
-    console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+    lang = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday";
+    console.log(lang);
+    arr = lang.split(", ");
+    numDey = numDey - 1;
+    console.log(arr[numDey]);
     break;
 }
 
 lang = prompt("Введите язык отображения", "ru,en");
-
+arr = [];
+arr['ru'] = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
+arr['en'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+console.log(arr[lang]);
 
 let namePerson = prompt("Введите имя", "Артем, Александр");
 let resalt = (namePerson == "Артем") ? "директор": (namePerson == "Александр") ? "учитель" : "студент";
