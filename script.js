bec3686e3d240e23c1db4bb863c96d6b0842f712
YudakovVV <skiff2140@gmail.com>
2022-03-31 20:00:33 +0400

    выполнено задание 4

--- script.js
+++ script.js
@@ -1,46 +1,106 @@
-let lang = prompt("Введите язык отображения", "ru,en");
-let arr;
-let numDey = +prompt("Введите номер дня недели", "1 - 7");
-if (lang == "ru"){
-  lang = "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье";
-  console.log(lang);
-  arr = lang.split(", ");
-  numDey = numDey - 1;
-  console.log(arr[numDey]);
-  
+"use strict";
+let title = prompt("Как называется ваш проект?");
+console.log(title);
+let screens = prompt(
+  "Как называется ваш проект?",
+  "Простые, Сложные, Интерактивные"
+);
+console.log(screens);
+let screenPrice = +prompt("Сколько будет стоять данная работа", "1200");
+let rollback = 20;
+let fullPrice = 30000;
+console.log(fullPrice);
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+console.log(adaptive);
+let lowerCase = screens.toLowerCase();
+let arr = lowerCase.split(",");
+let persInter = fullPrice * (rollback / 100);
+let service1;
+let service2;
+let servicePrice1 = 3000;
+let servicePrice2 = 4000;
+
+let questions = prompt(
+  "Какой дополнительный тип услуги нужен?",
+  "service1, service2"
+);
+let questions2 = prompt(
+  "Сколько это будет стоить?",
+  "servicePrice1, servicePrice2"
+);
+
+if (questions == "service1") {
+  service1 = console.log(questions);
+  if (questions2 == "servicePrice1") {
+    console.log(servicePrice1);
+  } else console.log(servicePrice2);
+} else service2 = console.log(questions);
+if (questions2 == "servicePrice1") {
+  console.log(servicePrice1);
+} else console.log(servicePrice2);
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+//console.log(screens.length);
+// console.log(screens);
+console.log(
+  "Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани"
+);
+// console.log(arr);
+// console.log(persInter);
+let servicePercentPrice = fullPrice - rollback;
+console.log(servicePercentPrice);
+
+const getRollbackMessage = function (price) {
+  if (price >= 30000) {
+    return "Даем скидку в 10%";
+  } else if (price >= 15000 && price <= 30000) {
+    return "Даем скидку в 5%";
+  } else if (price <= 15000 && price >= 0) {
+    return "Скидка не предусмотрена%";
+  } else return "Что то пошло не так";
+};
+let rollbackMessage = getRollbackMessage(fullPrice);
+console.log(rollbackMessage);
+
+const getAllServicePrice = function () {
+  return servicePrice1 + servicePrice2;
+};
+let allServicePrices = getAllServicePrice();
+console.log(allServicePrices);
+
+// const getFullPrice = function () {
+//   return screenPrice + allServicePrices;
+// };
+// fullPrice = getFullPrice();
+// console.log(fullPrice);
+
+function getFullPrice() {
+  return screenPrice + allServicePrices;
 }
-else 
-  lang = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday";
-  console.log(lang);
-  arr = lang.split(", ");
-  numDey = numDey - 1;
-  console.log(arr[numDey]); 
+fullPrice = getFullPrice();
+console.log(fullPrice);
 
-lang = prompt("Введите язык отображения", "ru,en");
-numDey = +prompt("Введите номер дня недели", "1 - 7");
-switch(lang){
-  case "ru": 
-    lang = "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье";
-    console.log(lang);
-    arr = lang.split(", ");
-    numDey = numDey - 1;
-    console.log(arr[numDey]);
-    break;
-  case "en": 
-    lang = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday";
-    console.log(lang);
-    arr = lang.split(", ");
-    numDey = numDey - 1;
-    console.log(arr[numDey]);
-    break;
-}
+const getTitle = function () {
+  title = title.toLowerCase();
+  return title.replace(/(^|\s)\S/g, function (a) {
+    return a.toUpperCase();
+  });
+};
+console.log(getTitle(title));
 
-lang = prompt("Введите язык отображения", "ru,en");
-arr = [];
-arr['ru'] = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
-arr['en'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-console.log(arr[lang]);
+const getServicePercentPrices = function () {
+  return fullPrice - rollback;
+};
+let servicePercentPrices = getServicePercentPrices();
+console.log(servicePercentPrices);
 
-let namePerson = prompt("Введите имя", "Артем, Александр");
-let resalt = (namePerson == "Артем") ? "директор": (namePerson == "Александр") ? "учитель" : "студент";
-console.log(resalt);
+console.clear();
+
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+};
+
+showTypeOf(screens);
+showTypeOf(rollbackMessage);
+showTypeOf(getServicePercentPrices());
