b506a1fb5f6ebd7f1512cb250c5bb4b01318763b
YudakovVV <skiff2140@gmail.com>
2022-03-31 22:03:49 +0400

    внесены измененения, условие заменено на функцию

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 "use strict";
 let title = prompt("Как называется ваш проект?");
-console.log(title);
 let screens = prompt(
   "Как называется ваш проект?",
   "Простые, Сложные, Интерактивные"
 );
-console.log(screens);
 let screenPrice = +prompt("Сколько будет стоять данная работа", "1200");
 let rollback = 20;
 let fullPrice = 30000;
-console.log(fullPrice);
 let adaptive = confirm("Нужен ли адаптив на сайте?");
-console.log(adaptive);
 let lowerCase = screens.toLowerCase();
 let arr = lowerCase.split(",");
 let persInter = fullPrice * (rollback / 100);
@@ -19,7 +15,6 @@ let service1;
 let service2;
 let servicePrice1 = 3000;
 let servicePrice2 = 4000;
-
 let questions = prompt(
   "Какой дополнительный тип услуги нужен?",
   "service1, service2"
@@ -29,27 +24,40 @@ let questions2 = prompt(
   "servicePrice1, servicePrice2"
 );
 
-if (questions == "service1") {
-  service1 = console.log(questions);
+// if (questions == "service1") {
+//   service1 = console.log(questions);
+//   if (questions2 == "servicePrice1") {
+//     console.log(servicePrice1);
+//   } else console.log(servicePrice2);
+// } else service2 = console.log(questions);
+// if (questions2 == "servicePrice1") {
+//   console.log(servicePrice1);
+// } else console.log(servicePrice2);
+
+const getServicePerformance = function(){
+  if (questions == "service1") {
+  service1 = questions;
+    if (questions2 == "servicePrice1") {
+      return servicePrice1;
+    } else return servicePrice2;
+  } else service2 = questions;
   if (questions2 == "servicePrice1") {
-    console.log(servicePrice1);
-  } else console.log(servicePrice2);
-} else service2 = console.log(questions);
-if (questions2 == "servicePrice1") {
-  console.log(servicePrice1);
-} else console.log(servicePrice2);
+    return servicePrice1;
+  } else return servicePrice2;
+};
+let servicePerformance = getServicePerformance();
 
 fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
 //console.log(screens.length);
 // console.log(screens);
-console.log(
-  "Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани"
-);
+// console.log(
+//   "Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани"
+// );
 // console.log(arr);
 // console.log(persInter);
 let servicePercentPrice = fullPrice - rollback;
-console.log(servicePercentPrice);
+// console.log(servicePercentPrice);
 
 const getRollbackMessage = function (price) {
   if (price >= 30000) {
@@ -61,13 +69,13 @@ const getRollbackMessage = function (price) {
   } else return "Что то пошло не так";
 };
 let rollbackMessage = getRollbackMessage(fullPrice);
-console.log(rollbackMessage);
+// console.log(rollbackMessage);
 
 const getAllServicePrice = function () {
   return servicePrice1 + servicePrice2;
 };
 let allServicePrices = getAllServicePrice();
-console.log(allServicePrices);
+// console.log(allServicePrices);
 
 // const getFullPrice = function () {
 //   return screenPrice + allServicePrices;
@@ -79,7 +87,7 @@ function getFullPrice() {
   return screenPrice + allServicePrices;
 }
 fullPrice = getFullPrice();
-console.log(fullPrice);
+// console.log(fullPrice);
 
 const getTitle = function () {
   title = title.toLowerCase();
@@ -95,7 +103,6 @@ const getServicePercentPrices = function () {
 let servicePercentPrices = getServicePercentPrices();
 console.log(servicePercentPrices);
 
-console.clear();
 
 const showTypeOf = function (variable) {
   console.log(variable, typeof variable);
