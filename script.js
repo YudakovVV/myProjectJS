bc5f1c4ffe0c5a611cc92459c9a844edc680e075
YudakovVV <skiff2140@gmail.com>
2022-04-07 21:51:44 +0400

    задание 7

--- script.js
+++ script.js
@@ -1,42 +1,97 @@
 "use strict";
 
+const appData = {
+  title: "",
+  screens: "",
+  screenPrice: 0,
+  adaptive: true,
+  rollback: 20,
+  allServicePrices: 0,
+  fullPrice: 0,
+  servicePercentPrices: 0,
+  service1: "",
+  service2: "",
+  servicePrice1: "",
+  servicePrice2: "",
 
-function game() {
-  let min = 1;
-  let max = 100;
-  let attempt = 10;
-  let result;
-  let num = Math.floor(Math.random() * (max - min)) + min;
-  console.log(num);
-  function run() {
-    let userNum = prompt("Угадай число от 1 до 100");
-    if (attempt < 1) {
-      alert("Попытки кончились! Игра окончена");
-    } else if (isNaN(userNum)) {
-      alert("Введите число!");
-      run();
+  start: function () {
+    appData.asking();
+    appData.allServicePrices = appData.getAllServicePrices();
+    appData.fullPrice = appData.getFullPrice();
+    appData.servicePercentPrices = appData.getServicePercentPrices();
+    appData.title = appData.getTitle();
+    appData.logger();
+  },
+  asking: function () {
+    appData.title = prompt("Как называется ваш проект?", "КалькуЛяТор вЕрсТки");
+    appData.screens = prompt(
+      "Какие типы экранов нужно разрабатывать?",
+      "Простые, Сложные, Интерактивные"
+    );
+
+    do {
+      appData.screenPrice = prompt("Сколько будет стоять данная работа?");
+    } while (!appData.isNumber(appData.screenPrice));
+    appData.screenPrice = Number(appData.screenPrice);
+
+    appData.adaptive = confirm("Нужен ли адаптив на сайте?");
+  },
+
+  isNumber: function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+  },
+
+  getAllServicePrices: function () {
+    for (let i = 0; i < 2; i++) {
+      if (i === 0) {
+        appData.service1 = prompt(
+          "Какой дополнительный тип услуги нужен?",
+          "Сверстать слайдер"
+        );
+        while (!appData.isNumber(appData.servicePrice1)) {
+          appData.servicePrice1 = prompt("Сколько это будет стоить?");
+        }
+      } else if (i === 1) {
+        appData.service2 = prompt(
+          "Какой дополнительный тип услуги нужен?",
+          "Блок с авторами"
+        );
+        while (!appData.isNumber(appData.servicePrice2)) {
+          appData.servicePrice2 = prompt("Сколько это будет стоить?");
+        }
+      }
     }
-    if (userNum === null) {
-      alert("Игра окончена, до свидания!");
+    return Number(appData.servicePrice1) + Number(appData.servicePrice2);
+  },
+
+  getFullPrice: function () {
+    return appData.screenPrice + appData.allServicePrices;
+  },
+
+  getServicePercentPrices: function () {
+    return appData.fullPrice - appData.fullPrice * (appData.rollback / 100);
+  },
+
+  getTitle: function () {
+    appData.title = appData.title.toLowerCase();
+    return appData.title.replace(/(^|\s)\S/g, function (a) {
+      return a.toUpperCase();
+    });
+  },
+
+  getRollbackMessage: function (price) {
+    if (price >= 30000) {
+      return "Даем скидку в 10%";
+    } else if (price >= 15000 && price <= 30000) {
+      return "Даем скидку в 5%";
+    } else if (price <= 15000 && price >= 0) {
+      return "Скидка не предусмотрена%";
+    } else return "Что то пошло не так";
+  },
+  logger: function () {
+    for (let key in appData) {
+      console.log("Ключ: " + key + " " + "Значение: " + appData[key]);
     }
-
-    if (parseInt(userNum) > num) {
-      attempt--;
-      alert("Загаданное число меньше, осталось попыток " + attempt);
-      run();
-    } else if (parseInt(userNum) < num) {
-      attempt--;
-      alert("Загаданное число больше, осталось попыток " + attempt);
-      run();
-    } else if (parseInt(userNum) === num) {
-      resalt = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
-      if(resalt === true) game();
-      else alert("Игра окончена, до свидания!");
-          
-    }
-  }
-  run();
-}
-game();
-
-
+  },
+};
+appData.start();
